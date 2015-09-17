61e6edf64f0ae3734895678960a3ab7bc584bb1b
Franktosh <franktosh@Franktoshs-iMac.local>
2015-09-16 23:30:57 -0700

    added some logic for error handling input that was not selected by user

--- javascript/app.js
+++ javascript/app.js
@@ -3,29 +3,58 @@ var main = function() {
 		major, 
 		course,
 		subject,
-		message;
+		message,
+		missingField;
 
 
-	function SectionMissing(){
-		$('#fillSection').append('Please fill in area with *.');
+	function SectionmissingField(check){
+		if(check === false){
+			$('#fillSection').empty();
+			$('#fillSection').append('Please fill in area with *.');
+		} else {
+			console.log('Inputs are complete');
+		}
+
+		
 	};
 
-	$("#button").click(function(){
-		//alert("button press");
-		console.log("button press")
-		if ($('#colleges').val() === "college")
+	$('#button').click(function(){
+		//alert('button press');
+		console.log('button press')
+		if ($('#colleges').val() === 'college')
 		{
-			console.log("Select a college");
-			// alert("Select a college");
+			missingField = false;
+			console.log('Select a college');
+			// alert('Select a college');
 			$('#collegeOption').empty();
-			$('#collegeOption').append("College*");
-			SectionMissing();
+			$('#collegeOption').append('College*');
+		}else{
+			$('#collegeOption').empty();
+			$('#collegeOption').append('College');
+		} 
 
+		if ($('#major').val() === 'major'){
+			missingField = false;
+			console.log('Select a major');
+			$('#majorOption').empty();
+			$('#majorOption').append('Major*');
+		} else{
+			$('#majorOption').empty();
+			$('#majorOption').append('Major');
 		}
-		else if {
-			
+
+		if($('#course').val() === 'course'){
+			missingField = false;
+			console.log('Select class');
+			$('#courseOption').empty();
+			$('#courseOption').append('Course*');
+		} else {
+			$('#courseOption').empty();
+			$('#courseOption').append('Course');
 		}
 		
+		SectionmissingField(missingField);
+
 	});
 }
 
