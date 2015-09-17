de435a6ae304134a6a75b1e4fd73b117a9158509
Franktosh <franktosh@Franktoshs-iMac.local>
2015-09-17 00:41:34 -0700

    check input is mostly complete need to check if information saved in variables properly

--- javascript/app.js
+++ javascript/app.js
@@ -4,12 +4,13 @@ var main = function() {
 		course,
 		subject,
 		message,
+		email,
 		missingField;
 
 
 	function SectionmissingField(check){
 		if(check === false){
-			$('#fillSection').empty();
+			$('#fillSection').empty(); //for when user does nor fill fields properly
 			$('#fillSection').append('Please fill in area with *.');
 		} else {
 			console.log('Inputs are complete');
@@ -18,6 +19,20 @@ var main = function() {
 		
 	};
 
+	function PostInformation () {
+		
+		// body...
+	}
+
+
+	//http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
+	function validateEmail(checkEmail) {
+    	var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    	return re.test(email);
+	}
+
+
+	//checks that all the fields have an input by user
 	$('#button').click(function(){
 		//alert('button press');
 		console.log('button press')
@@ -29,6 +44,7 @@ var main = function() {
 			$('#collegeOption').empty();
 			$('#collegeOption').append('College*');
 		}else{
+			college = $('#colleges').val();
 			$('#collegeOption').empty();
 			$('#collegeOption').append('College');
 		} 
@@ -39,6 +55,7 @@ var main = function() {
 			$('#majorOption').empty();
 			$('#majorOption').append('Major*');
 		} else{
+			major = $('#major').val();
 			$('#majorOption').empty();
 			$('#majorOption').append('Major');
 		}
@@ -49,13 +66,95 @@ var main = function() {
 			$('#courseOption').empty();
 			$('#courseOption').append('Course*');
 		} else {
+			course = $('#course').val();
 			$('#courseOption').empty();
 			$('#courseOption').append('Course');
 		}
 		
+		if($('#subject').val() === ''){
+			missingField = false;
+			$('#subjectTitle').empty();
+			$('#subjectTitle').append('Subject:*');
+		} else {
+			subject = $('#subject').val();
+		    $('#subjectTitle').empty();
+			$('#subjectTitle').append('Subject:');
+		}
+
+		if($('#email').val() !== ''){
+			email = $('#email').val();
+			if(validateEmail(email)){
+				$('#emailTitle').empty();
+				$('#emailTitle').append('Email(optional):');
+				console.log(email);
+				console.log('Email is correct & Saved');
+			}
+			else{
+				console.log('Enter a valid email');
+				missingField = false;
+				email = '';
+				$('#emailTitle').empty();
+				$('#emailTitle').append('Email(optional):*Enter valid email or leave blank');
+			}
+		} else{
+			email = '';
+			$('#emailTitle').empty();
+			$('#emailTitle').append('Email(optional):');
+		}
+
+		if (!$.trim($("#message").val())) {
+    		// textarea is empty or contains only white-space
+    		console.log('Empty text body');
+    		$('#messageTitle').empty();
+			$('#messageTitle').append('Message:*');
+		}
+		else{
+			message = $.trim($("#message").val());
+			$('#messageTitle').empty();
+			$('#messageTitle').append('Message:*');
+		}
+
+		// if(!$.trim($('#message')).val()){
+		// 	missingField = false;
+		// 	$('#messageTittle').empty();
+		// 	$('#messageTittle').append('Message:*');
+		// } else{
+		// 	message = $('#message').val();
+		// 	$('#messageTittle').empty();
+		// 	$('#messageTittle').append('Message:');
+		// }
+
 		SectionmissingField(missingField);
 
 	});
 }
 
 $(document).ready(main);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
